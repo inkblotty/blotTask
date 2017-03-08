0abaa820777c77cf2b79dfe67213e437cf694aa8
Katie Foster <inkblot.and.blush@gmail.com>
2017-03-07 20:40:54 -0700

    adding import file and entry point; may remove for stream

---
+++ src/import-user.js
@@ -0,0 +1,19 @@
+{
+	0001: {
+		username: 'Eliot Waugh',
+		tasks: [
+			{
+				description: 'Lunch meeting with Quentin',
+				datetime: 1489087800000,
+				length: 60,
+				style: 'red'
+			},
+			{
+				description: 'Gym',
+				datetime: 1489023900000,
+				length: 60,
+				style: 'red'
+			},
+		]
+	}
+}
