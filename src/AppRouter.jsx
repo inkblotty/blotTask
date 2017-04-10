e5c02a7cf6fe71a12f94c239d3615a94b00324d5
Katie Foster <inkblot.and.blush@gmail.com>
2017-04-10 12:19:20 -0600

    router and provider rework again

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { connect, Provider } from 'react-redux';
 
 import { createTask, updateTask, deleteTask, updateDisplayName } from './actions/actions';
 
@@ -9,40 +8,10 @@ import WelcomeContainer from './Containers/WelcomeContainer';
 
 const AppRouter = () => {
   return (
-    <Provider store={ store }>
       <Router history={ browserHistory }>
         <Route path='/' component={ WelcomeContainer } />
       </Router>
-    </Provider>
   );
 };
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    currentUser: state.currentUser,
-    tasks: state.tasks,
-    loading: state.loading
-  }
-}
-
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    createTask: (description, date, time, duration) => {
-      dispatch(createTask(description, date, time, duration))
-    },
-    updateTask: (taskId, description, date, time, duration) => {
-      dispatch(updateTask(taskId, description, date, time, duration))
-    },
-    deleteTask: (taskId, taskDate) => {
-      dispatch(deleteTask(taskId, taskDate))
-    },
-    updateDisplayName: (userId, newName)  => {
-      dispatch(updateDisplayName(userId, newName))
-    }
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(AppRouter);
+export default AppRouter;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect, Provider } from 'react-redux';
 
 import AppRouter from './AppRouter';
+import store from './store.js';
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'));
+const mapStateToProps = (state, ownProps) => {
+  return {
+    currentUser: state.currentUser,
+    tasks: state.tasks,
+    loading: state.loading
+  }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    createTask: (description, date, time, duration) => {
+      dispatch(createTask(description, date, time, duration))
+    },
+    updateTask: (taskId, description, date, time, duration) => {
+      dispatch(updateTask(taskId, description, date, time, duration))
+    },
+    deleteTask: (taskId, taskDate) => {
+      dispatch(deleteTask(taskId, taskDate))
+    },
+    updateDisplayName: (userId, newName)  => {
+      dispatch(updateDisplayName(userId, newName))
+    }
+  }
+}
+
+const Prov = () => {
+	<Provider store>
+		<AppRouter />
+	</Provider>
+}
+
+let app = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Prov);
+
+ReactDOM.render(app, document.getElementById('root'));
